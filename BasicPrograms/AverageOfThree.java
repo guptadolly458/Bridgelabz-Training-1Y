7eafdbc8b4e351b14418225454be63a77e3ddebe
guptadolly458 <gupta.dolly353@gmail.com>
2026-01-12 01:32:31 +0530

    AverageOfThree

---
+++ BasicPrograms/AverageOfThree.java
@@ -0,0 +1,10 @@
+public class AverageOfThree {
+    public static void main(String[] args) {
+        double a = 112;
+        double b = 203;
+        double c = 345;
+
+        double avg = (a + b + c) / 3;
+        System.out.println("Average = " + avg);
+    }
+}
---
+++ BasicPrograms/SimpleInterest.java
@@ -0,0 +1,10 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        double p = Double.parseDouble(args[0]);
+        double r = Double.parseDouble(args[1]);
+        double t = Double.parseDouble(args[2]);
+
+        double si = (p * r * t) / 100;
+        System.out.println("Simple Interest = " + si);
+    }
+}
