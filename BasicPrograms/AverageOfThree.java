b0775d8f7ef99e3b83b86febf23a5d231ffa656c
guptadolly458 <gupta.dolly353@gmail.com>
2026-01-12 12:41:08 +0530

    AverageOfThree

--- BasicPrograms/AverageOfThree.java
+++ BasicPrograms/AverageOfThree.java
@@ -1,8 +1,8 @@
 public class AverageOfThree {
     public static void main(String[] args) {
-        double a = 112;
-        double b = 203;
-        double c = 345;
+        double a = 67;
+        double b = 45;
+        double c = 34;
 
         double avg = (a + b + c) / 3;
         System.out.println("Average = " + avg);
