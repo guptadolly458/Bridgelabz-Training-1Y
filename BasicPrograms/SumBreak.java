d136e2660895c37b80047ef7df0ea06651e1b3ba
guptadolly458 <gupta.dolly353@gmail.com>
2026-01-21 23:24:40 +0530

    If-else

---
+++ BasicPrograms/SumBreak.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class SumBreak {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0;
+
+        while (true) {
+            double num = sc.nextDouble();
+
+            if (num <= 0) {
+                break;
+            } else {
+                total += num;
+            }
+        }
+        System.out.println("Total = " + total);
+    }
+}
---
+++ BasicPrograms/SumUntilZero.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0;
+        double num = sc.nextDouble();
+
+        while (num != 0) {
+            total += num;
+            num = sc.nextDouble();
+        }
+
+        System.out.println("Total = " + total);
+    }
+}
