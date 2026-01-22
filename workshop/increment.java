b984f497e3a710b12b91894be41e7d7b367b32f0
guptadolly458 <gupta.dolly353@gmail.com>
2026-01-22 15:22:04 +0530

    Workshop

---
+++ workshop/increment.java
@@ -0,0 +1,17 @@
+public class increment{
+    public static void main(String[] args){
+	    for (int i=0;i<10;i++){
+		    System.out.println("i:" +i);
+			}
+		int j=10;
+		while (j>0){
+			System.out.println("j:" +j);
+		}
+		int k=10;
+		do{
+			System.out.println("k:" +k);
+			k--;
+		}
+		
+	}
+	}
