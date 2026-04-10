05d855e228ef941663f0f313e26ee98be46e46bb
guptadolly458 <gupta.dolly353@gmail.com>
2026-04-10 11:46:24 +0530

    workshop

--- workshop/src/com/gla/Logistics/CheckpointDetails.java
+++ workshop/src/com/gla/Logistics/CheckpointDetails.java
@@ -1,4 +1,49 @@
 package Logistics;
 
 public class CheckpointDetails {
+    String checkpointId;
+    String locationName;
+    int distanceFromLast;
+    int expectedDuration;
+    int actualDuration;
+
+    public String getCheckpointId() {
+        return checkpointId;
+    }
+
+    public void setCheckpointId(String checkpointId) {
+        this.checkpointId = checkpointId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public int getDistanceFromLast() {
+        return distanceFromLast;
+    }
+
+    public void setDistanceFromLast(int distanceFromLast) {
+        this.distanceFromLast = distanceFromLast;
+    }
+
+    public int getExpectedDuration() {
+        return expectedDuration;
+    }
+
+    public void setExpectedDuration(int expectedDuration) {
+        this.expectedDuration = expectedDuration;
+    }
+
+    public int getActualDuration() {
+        return actualDuration;
+    }
+
+    public void setActualDuration(int actualDuration) {
+        this.actualDuration = actualDuration;
+    }
 }
--- workshop/src/com/gla/Logistics/DelayPenality.java
+++ workshop/src/com/gla/Logistics/DelayPenality.java
@@ -2,7 +2,20 @@ package Logistics;
 
 public class DelayPenality {
     static int Delivery{
-        if()
+        static int Delivery{
+            Scanner sc = new Scanner();
+            int actual;
+            int expected;
+            int Fuel;
+            float DelayPenality;
+            if(DelayPenality>30 Object min;
+            Object min1 = min;){
+                Delivery = (actual-expected)*0.5;
+                Fuel flat = Delivery / 0.01;
+            }
+        else{
+                System.out.println("No Penalty");
+            }
     }
 }
 
